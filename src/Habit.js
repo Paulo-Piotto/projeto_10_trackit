2c20603451af6cb227b25f6e93e0d8b6b9017375
Paulo <paulo_piotto@hotmail.com>
2021-09-12 11:03:28 -0300

    rota habitos finalizada

---
+++ src/Habit.js
@@ -0,0 +1,90 @@
+import styled from "styled-components";
+import { IoTrashOutline } from "react-icons/io5"
+import { useContext } from "react";
+import DayHabit from "./dayHabit";
+import UserContext from "./userContext";
+import axios from "axios";
+
+export default function Habit({habit, setHabits, habits}){
+    const week = [
+        { 
+            name: 'D',
+            id: 0 
+        },
+        { 
+            name: 'S',
+            id: 1 
+        },
+        { 
+            name: 'T',
+            id: 2 
+        },
+        { 
+            name: 'Q',
+            id: 3 
+        },
+        { 
+            name: 'Q',
+            id: 4 
+        },
+        { 
+            name: 'S',
+            id: 5 
+        },
+        { 
+            name: 'S',
+            id: 6 
+        },
+        
+       
+    ];
+    const user = useContext(UserContext);
+
+    function deleteHabit(){
+        const config = {
+            headers: {
+                Authorization: `Bearer ${user.token}`
+            }
+        }
+        axios.delete(`https://mock-api.bootcamp.respondeai.com.br/api/v2/trackit/habits/${habit.id}`, config)
+            .then(() => setHabits(habits.filter((el) =>
+                el.name === habit.name ? false : true
+            )))
+    }
+
+    return(
+        <HabitContainer>
+            <section>
+                <span>{habit.name}</span>
+                <IoTrashOutline onClick={deleteHabit} />
+            </section>
+            <div>
+                {week.map((day) => 
+                <DayHabit
+                days={habit.days}
+                day={day}
+                key={day.id}
+                />)}
+            </div>
+        </HabitContainer>
+    );
+}
+
+const HabitContainer = styled.div`
+    background-color: white;
+    width: 95%;
+    border-radius: 5px;
+    padding: 18px;
+    margin-bottom: 10px;
+
+    div{
+        display: flex;
+    }
+
+    section{
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        margin-bottom: 8px;
+    }
+`
--- src/habits.js
+++ src/habits.js
@@ -1,15 +1,48 @@
+import { useState, useEffect, useContext } from "react";
+import axios from "axios";
+import styled from "styled-components";
+import UserContext from "./userContext";
 import Header from "./header";
 import Footer from "./footer";
 import { Container } from "./history";
-import styled from "styled-components";
+import Habit from "./Habit";
+import CreateHabit from "./createHabit";
+
+
 
 export default function Habits(){
+
+    const user = useContext(UserContext);
+    const [habits, setHabits] = useState(null);
+    const [create, setCreate] = useState(false);
+
+    const config = {
+        headers: {
+            Authorization: `Bearer ${user.token}`
+        }
+    }
+
+
+    useEffect(() => {
+        axios.get('https://mock-api.bootcamp.respondeai.com.br/api/v2/trackit/habits', config)
+            .then((resp) => {
+                setHabits([...resp.data])
+            })
+    }, [])
+
+
     return (
     <>
         <Header />
         <Container>
+            <Content>
             <h3>Meus Hábitos</h3>
-            <AddButton>+</AddButton>
+            <AddButton onClick={() => setCreate(true)}>+</AddButton>
+            <CreateHabit create={create} setCreate={setCreate} setHabits={setHabits} habits={habits}/>
+            {habits ? habits.map((habit) => <Habit setHabits={setHabits} habits={habits} key={habit.id} habit={habit} />)
+            :<p>Você não tem nenhum hábito cadastrado ainda. Adicione um hábito para começar a trackear!</p>
+            }
+            </Content>
         </Container>
         <Footer />
     </>
@@ -28,4 +61,8 @@ const AddButton = styled.button`
     position: absolute;
     right: 18px;
     top: 85px;
+`
+
+const Content = styled.div`
+    margin-bottom: 80px;
 `
